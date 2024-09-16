775407f6580cf2a4cf4b9ba75f60051001195025
Davide Canton <davide.canton5@gmail.com>
2024-09-16 15:27:58 +0200

    removed comments

--- pytest-mycompletion.bash
+++ pytest-mycompletion.bash
@@ -3,13 +3,9 @@ _pytest_suggest_completions() {
     local cur
     _get_comp_words_by_ref -n : cur
 
-    # if [ "${#COMP_WORDS[@]}" != "2" ]; then
-    #     return
-    # fi
-
     COMPREPLY=($(compgen -W '$(pytest-suggest "$cur")' -- "$cur"))
 
     __ltrim_colon_completions "$cur"
 }
 
-complete -F _pytest_suggest_completions pytest
+complete -o bashdefault -o default -F _pytest_suggest_completions pytest
